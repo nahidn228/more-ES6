7e5d24ea1b6ba667b5ee10684196de13fe8a9b37
Nahid Hasan <nahidn228@gmail.com>
2024-09-27 19:53:14 +0600

    reduce is not complete

--- practice.js
+++ practice.js
@@ -19,6 +19,8 @@ let data = {
   },
 };
 
+//console.log(data.Sophia.study[1].secondary[1]);
+
 // task-2: console .log  output: Petersburg, Herry
 
 let students = {
@@ -46,6 +48,8 @@ let students = {
   },
 };
 
+//console.log(students["2222"].address.city, students["3333"].name);
+
 // task-3 : access and then show habluder adda
 // show output Beginner
 let data2 = {
@@ -71,6 +75,66 @@ let data2 = {
   ],
 };
 
+//console.log(data2.data[0].bookDetails.name);
+
+// Problem: 01
+
+const oddArray = [1, 3, 5, 7, 9, 11];
+
+let evenArray = [];
+
+for (let odd of oddArray) {
+  odd = odd + 1;
+  evenArray.push(odd);
+}
+//console.log(evenArray);
+
+const evenNumbers = (num) => num + 1;
+const result = oddArray.map(evenNumbers);
+//console.log(result);
+
+// Problem: 02
+
+const numbers = [50, 20, 25, 27, 30, 35, 78, 33, 90, 33, 101, 100];
+const getNumber = numbers.filter((num) => num % 10 == 0);
+const getNum = numbers.find((num) => num % 10 == 0);
+// console.log(getNumber);
+// console.log(getNum);
+
+// Problem: 03
+
+const instructor = [
+  { name: "Nodi", age: 28, position: "Senior" },
+  { name: "Akil", age: 26, position: "junior" },
+  { name: "Sobuj", age: 30, position: "Senior" },
+];
+
+// console.log(instructor[0].position);
+// const result1 = instructor[0].position.toLowerCase();
+const seniorInstructor = instructor.filter((n) => n.position == "Senior");
+
+//console.log(seniorInstructor);
+
+// Problem: 04
+
+const peoples = [
+  { name: "Mina", age: 20 },
+  { name: "Rina", age: 15 },
+  { name: "Suchorita", age: 20 },
+  { name: "Sonchita", age: 15 },
+];
+
+let sum = 0;
+for (person of peoples) {
+  //sum += person.age;
+  sum = sum + person.age;
+}
+//console.log(sum);
+
+
+
+/*
+// Quiz question
 const nums = [1, 2, 3, 4, 5];
 let output = nums.filter((n) => n % 2);
 console.log(output);
@@ -88,3 +152,4 @@ console.log(min([1, 3, 2]));
 
 const cube = (x) => x * x * x;
 console.log(cube(2));
+*/
